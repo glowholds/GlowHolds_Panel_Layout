5687b2c52b988edbdda388203796234bb14d6446
Chris Parsons <chris.parsons@me.com>
2017-01-22 14:21:47 -0800

    Initial Commit

---
+++ GlowHoldsPanelLayout/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GlowHoldsPanelLayout
+//
+//  Created by Chris Parsons on 1/22/17.
+//  Copyright © 2017 Rectec. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GlowHoldsPanelLayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GlowHoldsPanelLayout
+//
+//  Created by Chris Parsons on 1/22/17.
+//  Copyright © 2017 Rectec. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
